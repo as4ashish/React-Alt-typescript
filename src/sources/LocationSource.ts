669d7f8f65f99fb0394f4f9f6d260da0c953a906
Ashish Kumar <41803696+as4ashish@users.noreply.github.com>
2019-08-26 22:51:42 +0530

    Remove fetching data from API
    Removed fetching data from API, instead using and array and mimic API call

--- src/sources/LocationSource.ts
+++ src/sources/LocationSource.ts
@@ -1,43 +1,30 @@
 var LocationActions = require("../actions/LocationActions");
 import { hot } from "react-hot-loader";
 
-var mockData = "https://localhost:44311/api/locations";
-// [
-//   { id: 0, name: "Abu Dhabi" },
-//   { id: 1, name: "Berlin" },
-//   { id: 2, name: "Bogota" },
-//   { id: 3, name: "Buenos Aires" },
-//   { id: 4, name: "Cairo" },
-//   { id: 5, name: "Chicago" },
-//   { id: 6, name: "Lima" },
-//   { id: 7, name: "London" },
-//   { id: 8, name: "Miami" },
-//   { id: 9, name: "Moscow" },
-//   { id: 10, name: "Mumbai" },
-//   { id: 11, name: "Paris" },
-//   { id: 12, name: "San Francisco" }
-// ];
-
-function readJson() {
-  return fetch(mockData);
-}
+var mockData = [
+  { id: 0, name: "Abu Dhabi" },
+  { id: 1, name: "Berlin" },
+  { id: 2, name: "Bogota" },
+  { id: 3, name: "Buenos Aires" },
+  { id: 4, name: "Cairo" },
+  { id: 5, name: "Chicago" },
+  { id: 6, name: "Lima" },
+  { id: 7, name: "London" },
+  { id: 8, name: "Miami" },
+  { id: 9, name: "Moscow" },
+  { id: 10, name: "Mumbai" },
+  { id: 11, name: "Paris" },
+  { id: 12, name: "San Francisco" }
+];
 
 var LocationSource = {
-  fetchLocations(mockData) {
+  fetchLocations() {
     return {
       remote() {
         return new Promise(function(resolve, reject) {
           setTimeout(function() {
             if (true) {
-              resolve(
-                readJson()
-                  .then(function(response) {
-                    return response.json();
-                  })
-                  .then(function(myJson) {
-                    return myJson;
-                  })
-              );
+              resolve(mockData);
             } else {
               reject("Things have broken");
             }
@@ -49,9 +36,9 @@ var LocationSource = {
         return null;
       },
 
-      success: LocationActions.updateLocations
-      //error: LocationActions.locationsFailed,
-      //loading: LocationActions.fetchLocations
+      success: LocationActions.updateLocations,
+      error: LocationActions.locationsFailed,
+      loading: LocationActions.fetchLocations
     };
   }
 };
